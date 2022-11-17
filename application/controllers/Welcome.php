fd85c58abbe754170d8e9982a25684477f266bf2
Thiago Moura <thiago.moura2000@hotmail.com>
2022-11-17 14:50:35 -0300

    Início da criação do portal CLIENTE

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -4,7 +4,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 class Welcome extends CI_Controller {
 
 	public function index() {
-		$this->load->view('welcome_message');
+		$this->load->view('web/loja');
 	}
 
 }
