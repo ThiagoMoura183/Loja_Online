c49287d47d98825e1d5a1797b90b352e70844292
Thiago Moura <thiago.moura2000@hotmail.com>
2022-11-17 15:14:58 -0300

    Realizado todo fatiamento do template para acesso portal CLIENTE

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -4,7 +4,10 @@ defined('BASEPATH') or exit('No direct script access allowed');
 class Welcome extends CI_Controller {
 
 	public function index() {
+
+		$this->load->view('web/layout/header');
 		$this->load->view('web/loja');
+		$this->load->view('web/layout/footer');
 	}
 
 }
---
+++ application/views/web/layout/header.php
@@ -0,0 +1,47 @@
+<!doctype html>
+<html class="no-js" lang="zxx">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>Home Version One || limupa - Digital Products Store eCommerce Bootstrap 4 Template</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Favicon -->
+    <link rel="shortcut icon" type="image/x-icon" href="images/favicon.png">
+    <!-- Material Design Iconic Font-V2.2.0 -->
+    <link rel="stylesheet" href="<?php echo base_url('public/web/css/material-design-iconic-font.min.css') ?>">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="<?php echo base_url('public/web/css/font-awesome.min.css') ?>">
+    <!-- Font Awesome Stars-->
+    <link rel="stylesheet" href="<?php echo base_url('public/web/css/fontawesome-stars.css') ?>">
+    <!-- Meanmenu CSS -->
+    <link rel="stylesheet" href="<?php echo base_url('public/web/css/meanmenu.css') ?>">
+    <!-- owl carousel CSS -->
+    <link rel="stylesheet" href="<?php echo base_url('public/web/css/owl.carousel.min.css') ?>">
+    <!-- Slick Carousel CSS -->
+    <link rel="stylesheet" href="<?php echo base_url('public/web/css/slick.css') ?>">
+    <!-- Animate CSS -->
+    <link rel="stylesheet" href="<?php echo base_url('public/web/css/animate.css') ?>">
+    <!-- Jquery-ui CSS -->
+    <link rel="stylesheet" href="<?php echo base_url('public/web/css/jquery-ui.min.css') ?>">
+    <!-- Venobox CSS -->
+    <link rel="stylesheet" href="<?php echo base_url('public/web/css/venobox.css') ?>">
+    <!-- Nice Select CSS -->
+    <link rel="stylesheet" href="<?php echo base_url('public/web/css/nice-select.css') ?>">
+    <!-- Magnific Popup CSS -->
+    <link rel="stylesheet" href="<?php echo base_url('public/web/css/magnific-popup.css') ?>">
+    <!-- Bootstrap V4.1.3 Fremwork CSS -->
+    <link rel="stylesheet" href="<?php echo base_url('public/web/css/bootstrap.min.css') ?>">
+    <!-- Helper CSS -->
+    <link rel="stylesheet" href="<?php echo base_url('public/web/css/helper.css') ?>">
+    <!-- Main Style CSS -->
+    <link rel="stylesheet" href="<?php echo base_url('public/web/css/style.css') ?>">
+    <!-- Responsive CSS -->
+    <link rel="stylesheet" href="<?php echo base_url('public/web/css/responsive.css') ?>">
+    <!-- Modernizr js -->
+    <script src="<?php echo base_url('public/web/js/vendor/modernizr-2.8.3.min.js') ?>"></script>
+</head>
+
+<body>
+    <div class="body-wrapper">
