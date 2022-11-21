e5f4696675cd5ec48065b568aa55628c30da087f
Thiago Moura <thiago.moura2000@hotmail.com>
2022-11-21 20:01:29 -0300

    Criação da View de Produto (Seleção Única) e início da edição dos campos

---
+++ application/views/web/produto.php
@@ -0,0 +1,267 @@
+<?php $this->load->view('web/layout/navbar'); ?>
+
+<!-- Begin Li's Breadcrumb Area -->
+<div class="breadcrumb-area">
+    <div class="container">
+        <div class="breadcrumb-content">
+            <ul>
+                <li><a href="<?php echo base_url('/') ?>">Home</a></li>
+                <li><a href="<?php echo base_url('master/' . $produto->categoria_pai_meta_link) ?>"><?php echo $produto->categoria_pai_nome ?></a></li>
+                <li class="active"><a href="<?php echo base_url('master/' . $produto->categoria_meta_link) ?>"><?php echo $produto->categoria_nome ?></a></li>
+            </ul>
+        </div>
+    </div>
+</div>
+<!-- Li's Breadcrumb Area End Here -->
+<!-- content-wraper start -->
+<div class="content-wraper">
+    <div class="container">
+        <div class="row single-product-area">
+            <div class="col-lg-5 col-md-6">
+                <!-- Product Details Left -->
+                <div class="product-details-left">
+                    <div class="product-details-images slider-navigation-1">
+                        <?php foreach ($fotos_produtos as $foto) : ?>
+                            <div class="lg-image">
+                                <a class="popup-img venobox vbox-item" href="<?php echo base_url('uploads/produtos/small/' . $foto->foto_caminho) ?>" data-gall="myGallery">
+                                    <img src="<?php echo base_url('uploads/produtos/' . $foto->foto_caminho) ?>" alt="<?php echo $produto->produto_nome ?>">
+                                </a>
+                            </div>
+                        <?php endforeach; ?>
+                    </div>
+
+                    <div class="product-details-thumbs slider-thumbs-1">
+                        <?php foreach ($fotos_produtos as $foto) : ?>
+                            <div class="sm-image"><img src="<?php echo base_url('uploads/produtos/small/' . $foto->foto_caminho) ?>" alt="<?php echo $produto->produto_nome ?>"></div>
+                        <?php endforeach; ?>
+                    </div>
+
+                </div>
+                <!--// Product Details Left -->
+            </div>
+
+            <div class="col-lg-7 col-md-6">
+                <div class="product-details-view-content pt-60">
+                    <div class="product-info">
+                        <h2>Today is a good day Framed poster</h2>
+                        <span class="product-details-ref">Reference: demo_15</span>
+                        <div class="rating-box pt-20">
+                            <ul class="rating rating-with-review-item">
+                                <li><i class="fa fa-star-o"></i></li>
+                                <li><i class="fa fa-star-o"></i></li>
+                                <li><i class="fa fa-star-o"></i></li>
+                                <li class="no-star"><i class="fa fa-star-o"></i></li>
+                                <li class="no-star"><i class="fa fa-star-o"></i></li>
+                                <li class="review-item"><a href="#">Read Review</a></li>
+                                <li class="review-item"><a href="#">Write Review</a></li>
+                            </ul>
+                        </div>
+                        <div class="price-box pt-20">
+                            <span class="new-price new-price-2">$57.98</span>
+                        </div>
+                        <div class="product-desc">
+                            <p>
+                                <span>100% cotton double printed dress. Black and white striped top and orange high waisted skater skirt bottom. Lorem ipsum dolor sit amet, consectetur adipisicing elit. quibusdam corporis, earum facilis et nostrum dolorum accusamus similique eveniet quia pariatur.
+                                </span>
+                            </p>
+                        </div>
+                        <div class="product-variants">
+                            <div class="produt-variants-size">
+                                <label>Dimension</label>
+                                <select class="nice-select">
+                                    <option value="1" title="S" selected="selected">40x60cm</option>
+                                    <option value="2" title="M">60x90cm</option>
+                                    <option value="3" title="L">80x120cm</option>
+                                </select>
+                            </div>
+                        </div>
+                        <div class="single-add-to-cart">
+                            <form action="#" class="cart-quantity">
+                                <div class="quantity">
+                                    <label>Quantity</label>
+                                    <div class="cart-plus-minus">
+                                        <input class="cart-plus-minus-box" value="1" type="text">
+                                        <div class="dec qtybutton"><i class="fa fa-angle-down"></i></div>
+                                        <div class="inc qtybutton"><i class="fa fa-angle-up"></i></div>
+                                    </div>
+                                </div>
+                                <button class="add-to-cart" type="submit">Add to cart</button>
+                            </form>
+                        </div>
+                        <div class="product-additional-info pt-25">
+                            <a class="wishlist-btn" href="wishlist.html"><i class="fa fa-heart-o"></i>Add to wishlist</a>
+                            <div class="product-social-sharing pt-25">
+                                <ul>
+                                    <li class="facebook"><a href="#"><i class="fa fa-facebook"></i>Facebook</a></li>
+                                    <li class="twitter"><a href="#"><i class="fa fa-twitter"></i>Twitter</a></li>
+                                    <li class="google-plus"><a href="#"><i class="fa fa-google-plus"></i>Google +</a></li>
+                                    <li class="instagram"><a href="#"><i class="fa fa-instagram"></i>Instagram</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="block-reassurance">
+                            <ul>
+                                <li>
+                                    <div class="reassurance-item">
+                                        <div class="reassurance-icon">
+                                            <i class="fa fa-check-square-o"></i>
+                                        </div>
+                                        <p>Security policy (edit with Customer reassurance module)</p>
+                                    </div>
+                                </li>
+                                <li>
+                                    <div class="reassurance-item">
+                                        <div class="reassurance-icon">
+                                            <i class="fa fa-truck"></i>
+                                        </div>
+                                        <p>Delivery policy (edit with Customer reassurance module)</p>
+                                    </div>
+                                </li>
+                                <li>
+                                    <div class="reassurance-item">
+                                        <div class="reassurance-icon">
+                                            <i class="fa fa-exchange"></i>
+                                        </div>
+                                        <p> Return policy (edit with Customer reassurance module)</p>
+                                    </div>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- content-wraper end -->
+<!-- Begin Product Area -->
+<div class="product-area pt-35">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="li-product-tab">
+                    <ul class="nav li-product-menu">
+                        <li><a class="active" data-toggle="tab" href="#description"><span>Description</span></a></li>
+                        <li><a data-toggle="tab" href="#product-details"><span>Product Details</span></a></li>
+                        <li><a data-toggle="tab" href="#reviews"><span>Reviews</span></a></li>
+                    </ul>
+                </div>
+                <!-- Begin Li's Tab Menu Content Area -->
+            </div>
+        </div>
+        <div class="tab-content">
+            <div id="description" class="tab-pane active show" role="tabpanel">
+                <div class="product-description">
+                    <span>The best is yet to come! Give your walls a voice with a framed poster. This aesthethic, optimistic poster will look great in your desk or in an open-space office. Painted wooden frame with passe-partout for more depth.</span>
+                </div>
+            </div>
+            <div id="product-details" class="tab-pane" role="tabpanel">
+                <div class="product-details-manufacturer">
+                    <a href="#">
+                        <img src="images/product-details/1.jpg" alt="Product Manufacturer Image">
+                    </a>
+                    <p><span>Reference</span> demo_7</p>
+                    <p><span>Reference</span> demo_7</p>
+                </div>
+            </div>
+            <div id="reviews" class="tab-pane" role="tabpanel">
+                <div class="product-reviews">
+                    <div class="product-details-comment-block">
+                        <div class="comment-review">
+                            <span>Grade</span>
+                            <ul class="rating">
+                                <li><i class="fa fa-star-o"></i></li>
+                                <li><i class="fa fa-star-o"></i></li>
+                                <li><i class="fa fa-star-o"></i></li>
+                                <li class="no-star"><i class="fa fa-star-o"></i></li>
+                                <li class="no-star"><i class="fa fa-star-o"></i></li>
+                            </ul>
+                        </div>
+                        <div class="comment-author-infos pt-25">
+                            <span>HTML 5</span>
+                            <em>01-12-18</em>
+                        </div>
+                        <div class="comment-details">
+                            <h4 class="title-block">Demo</h4>
+                            <p>Plaza</p>
+                        </div>
+                        <div class="review-btn">
+                            <a class="review-links" href="#" data-toggle="modal" data-target="#mymodal">Write Your Review!</a>
+                        </div>
+                        <!-- Begin Quick View | Modal Area -->
+                        <div class="modal fade modal-wrapper" id="mymodal">
+                            <div class="modal-dialog modal-dialog-centered" role="document">
+                                <div class="modal-content">
+                                    <div class="modal-body">
+                                        <h3 class="review-page-title">Write Your Review</h3>
+                                        <div class="modal-inner-area row">
+                                            <div class="col-lg-6">
+                                                <div class="li-review-product">
+                                                    <img src="images/product/large-size/3.jpg" alt="Li's Product">
+                                                    <div class="li-review-product-desc">
+                                                        <p class="li-product-name">Today is a good day Framed poster</p>
+                                                        <p>
+                                                            <span>Beach Camera Exclusive Bundle - Includes Two Samsung Radiant 360 R3 Wi-Fi Bluetooth Speakers. Fill The Entire Room With Exquisite Sound via Ring Radiator Technology. Stream And Control R3 Speakers Wirelessly With Your Smartphone. Sophisticated, Modern Design </span>
+                                                        </p>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="col-lg-6">
+                                                <div class="li-review-content">
+                                                    <!-- Begin Feedback Area -->
+                                                    <div class="feedback-area">
+                                                        <div class="feedback">
+                                                            <h3 class="feedback-title">Our Feedback</h3>
+                                                            <form action="#">
+                                                                <p class="your-opinion">
+                                                                    <label>Your Rating</label>
+                                                                    <span>
+                                                                        <select class="star-rating">
+                                                                            <option value="1">1</option>
+                                                                            <option value="2">2</option>
+                                                                            <option value="3">3</option>
+                                                                            <option value="4">4</option>
+                                                                            <option value="5">5</option>
+                                                                        </select>
+                                                                    </span>
+                                                                </p>
+                                                                <p class="feedback-form">
+                                                                    <label for="feedback">Your Review</label>
+                                                                    <textarea id="feedback" name="comment" cols="45" rows="8" aria-required="true"></textarea>
+                                                                </p>
+                                                                <div class="feedback-input">
+                                                                    <p class="feedback-form-author">
+                                                                        <label for="author">Name<span class="required">*</span>
+                                                                        </label>
+                                                                        <input id="author" name="author" value="" size="30" aria-required="true" type="text">
+                                                                    </p>
+                                                                    <p class="feedback-form-author feedback-form-email">
+                                                                        <label for="email">Email<span class="required">*</span>
+                                                                        </label>
+                                                                        <input id="email" name="email" value="" size="30" aria-required="true" type="text">
+                                                                        <span class="required"><sub>*</sub> Required fields</span>
+                                                                    </p>
+                                                                    <div class="feedback-btn pb-15">
+                                                                        <a href="#" class="close" data-dismiss="modal" aria-label="Close">Close</a>
+                                                                        <a href="#">Submit</a>
+                                                                    </div>
+                                                                </div>
+                                                            </form>
+                                                        </div>
+                                                    </div>
+                                                    <!-- Feedback Area End Here -->
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- Quick View | Modal Area End Here -->
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Product Area End Here -->
