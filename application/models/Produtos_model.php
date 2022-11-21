b5a9d87abea476afd1cf6a9c5922d318819c8940
Thiago Moura <thiago.moura2000@hotmail.com>
2022-11-21 20:00:48 -0300

    Criação de function para obter produto por Meta Link na exibição de produtos destaque

--- application/models/Produtos_model.php
+++ application/models/Produtos_model.php
@@ -4,6 +4,8 @@ defined('BASEPATH') or exit('Ação não permitida!');
 
 
 class Produtos_model extends CI_Model {
+
+    // O GetAll é utilizado na área restrita apenas
     public function getAll() {
         $this->db->select([
             'produtos.produto_id',
@@ -19,5 +21,34 @@ class Produtos_model extends CI_Model {
         $this->db->join('categorias', 'categorias.categoria_id = produtos.produto_categoria_id', 'LEFT');
         $this->db->join('marcas', 'marcas.marca_id = produtos.produto_marca_id', 'LEFT');
         return $this->db->get('produtos')->result();
+    }
+
+    // Recuperar o produto para detalhá-lo na página do cliente
+    public function getById($produto_meta_link = NULL) {
+        $this->db->select([
+            'produtos.produto_id',
+            'produtos.produto_codigo',
+            'produtos.produto_nome',
+            'produtos.produto_valor',
+            'produtos.produto_quantidade_estoque',
+            'produtos.produto_descricao',
+            'produtos.produto_meta_link',
+            'categorias_pai.categoria_pai_nome',
+            'categorias_pai.categoria_pai_meta_link',
+            'categorias.categoria_id',
+            'categorias.categoria_nome',
+            'categorias.categoria_meta_link',
+            'marcas.marca_id',
+            'marcas.marca_nome',
+            'marcas.marca_meta_link',
+        ]);
+
+        $this->db->where('produtos.produto_meta_link', $produto_meta_link);
+        $this->db->join('marcas', 'marcas.marca_id = produtos.produto_marca_id');
+        $this->db->join('categorias', 'categorias.categoria_id = produtos.produto_categoria_id');
+        $this->db->join('categorias_pai', 'categorias_pai.categoria_pai_id = categorias.categoria_pai_id');
+
+        return $this->db->get('produtos')->row();
+
     }
 }
