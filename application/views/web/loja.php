f0be30c68a32cbf00a6b50947cf68c37a5438e3f
Thiago Moura <thiago.moura2000@hotmail.com>
2022-11-21 20:01:08 -0300

    Edição na view para direcionar à view do produto selecionado

--- application/views/web/loja.php
+++ application/views/web/loja.php
@@ -76,7 +76,7 @@
                 <div class="li-product-tab">
                     <ul class="nav li-product-menu">
                         <li><a class="active" data-toggle="tab" href="#Mais-Vendidos"><span>Mais Vendidos</span></a></li>
-                        <li><a data-toggle="tab" href="#li-bestseller-product"><span>...</span></a></li>
+                        <li><a data-toggle="tab" href="#li-bestseller-product"><span>Novos Produtos (Em Breve)</span></a></li>
                         <li><a data-toggle="tab" href="#li-featured-product"><span>...</span></a></li>
                     </ul>
                 </div>
@@ -93,10 +93,10 @@
                         <div class="col-lg-12">
                             <div class="single-product-wrap">
                                 <div class="product-image">
-                                    <a href="single-product.html">
+                                    <a href="<?php echo base_url('produto/' . $prodDestaque->produto_meta_link )?>">
                                         <img src="<?php echo base_url('./uploads/produtos/' . $prodDestaque->foto_caminho)?>" alt="<?php echo word_limiter($prodDestaque->produto_nome,20) ?>">
                                     </a>
-                                    <span class="sticker">New</span>
+                                    <span class="sticker">UP</span>
                                 </div>
                                 <div class="product_desc">
                                     <div class="product_desc_info">
@@ -114,16 +114,14 @@
                                                 </ul>
                                             </div>
                                         </div>
-                                        <h4><a class="product_name" href="single-product.html"><?php echo word_limiter($prodDestaque->produto_nome,20) ?></a></h4>
+                                        <h4><a class="product_name" href="<?php echo base_url('produto/' . $prodDestaque->produto_meta_link )?>"><?php echo word_limiter($prodDestaque->produto_nome,20) ?></a></h4>
                                         <div class="price-box">
                                             <span class="new-price"><?php echo 'R$&nbsp' . number_format($prodDestaque->produto_valor,2)?></span>
                                         </div>
                                     </div>
                                     <div class="add-actions">
                                         <ul class="add-actions-link">
-                                            <li class="add-cart active"><a href="#">Add to Cart</a></li>
-                                            <li><a class="links-details" href="wishlist.html"><i class="fa fa-heart-o"></i></a></li>
-                                            <li><a href="#" title="quick view" class="quick-view-btn" data-toggle="modal" data-target="#exampleModalCenter"><i class="fa fa-eye"></i></a></li>
+                                            <li class="add-cart active"><a href="<?php echo base_url('produto/' . $prodDestaque->produto_meta_link )?>">Visualizar</a></li>
                                         </ul>
                                     </div>
                                 </div>
