f7056429a9930d3efee9dafcf0c0f4cc00c6cf17
Thiago Moura <thiago.moura2000@hotmail.com>
2022-11-21 19:58:01 -0300

    Criação da controller Produto para chamada de view

---
+++ application/controllers/Produto.php
@@ -0,0 +1,37 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Produto extends CI_Controller {
+
+    public function __construct() {
+        parent::__construct();
+    }
+
+    public function index($produto_meta_link = NULL) {
+
+        if (!$produto_meta_link || !$produto = $this->produtos_model->getById($produto_meta_link)) {
+            redirect('/');
+        } else {
+
+            $data = [
+                'titulo' => 'Visualizar Produto',
+                'scripts' => [
+                    'mask/jquery.mask.min.js',
+                    'mask/custom.js',
+                ],
+                'produto' => $produto
+            ];
+
+            $data['fotos_produtos'] = $this->core_model->getAll('produtos_fotos', ['foto_produto_id' => $produto->produto_id]);
+
+            // echo '<pre>';
+            // print_r($data);
+            // exit();
+
+
+            $this->load->view('web/layout/header', $data);
+            $this->load->view('web/produto');
+            $this->load->view('web/layout/footer');
+        }
+    }
+}
