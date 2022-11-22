debbf65b578605fb24d11db69953a35cf1208b10
Thiago Moura <thiago.moura2000@hotmail.com>
2022-11-21 21:18:58 -0300

    Criada função para obter todos os registros do produto agrupadas

--- application/models/Produtos_model.php
+++ application/models/Produtos_model.php
@@ -49,6 +49,31 @@ class Produtos_model extends CI_Model {
         $this->db->join('categorias_pai', 'categorias_pai.categoria_pai_id = categorias.categoria_pai_id');
 
         return $this->db->get('produtos')->row();
+    }
 
+    public function getAllBy($condicoes = NULL){
+        $this->db->select([
+            'produtos.produto_nome',
+            'produtos.produto_valor',
+            'produtos.produto_meta_link',
+            'categorias_pai.categoria_pai_nome',
+            'categorias.categoria_nome',
+            'produtos_fotos.foto_caminho'
+        ]);
+
+        $this->db->where('produtos.produto_ativo', 1);
+
+        if ($condicoes && is_array($condicoes)) {
+            $this->db->where($condicoes);
+        }
+
+        $this->db->join('categorias', 'categorias.categoria_id = produtos.produto_categoria_id', 'LEFT');
+        $this->db->join('marcas', 'marcas.marca_id = produtos.produto_marca_id', 'LEFT');
+        $this->db->join('categorias_pai', 'categorias_pai.categoria_pai_id = categorias.categoria_pai_id');
+        $this->db->join('produtos_fotos', 'produtos_fotos.foto_produto_id = produtos.produto_id', 'LEFT');
+
+        $this->db->group_by('produtos.produto_id');
+
+        return $this->db->get('produtos')->result();
     }
 }
